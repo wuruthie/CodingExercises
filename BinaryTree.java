18a3671a0e45ef6c5ab904184cd63d1cde505396
wuruthie <wuruth17@grinnell.edu>
2016-07-25 22:41:22 -0500

    add BinaryTree traversals

--- BinaryTree.java
+++ BinaryTree.java
@@ -15,25 +15,73 @@ public class BinaryTree{
     root = null;
   }
 
-  public Node insertNode(Node node, int val){
-    if(root == null){
-       root = new Node(val);
-       return root;
-
-    if(root.value < val){
-      root.left = insertNode(root.left,val);
-    } else if(root.value > val){
-      root.right = insertNode(root.right, val);
+  public Node addNode(Node node, int val){
+    if(node == null){
+       node = new Node(val);
+     } else {
+        if(node.data < val){
+          node.right = addNode(node.right,val);
+        } else if(node.data > val){
+          node.left = addNode(node.left, val);
+        }
+      }
+      return node;
     }
-    return root;
-  }
 
-  public void insert(int val){
-    root = insertNode(root, val);
+  public void add(int val){
+    root = addNode(root, val);
   }
 
   public boolean isLeaf(Node node){
     return(node.left == null && node.right == null);
   }
 
+  public void preOrderTraversal(Node node){
+    if(node == null){
+      return;
+    }else{
+      visit(node);
+      preOrderTraversal(node.left);
+      preOrderTraversal(node.right);
+    }
+  }
+
+  public void inOrderTraversal(Node node){
+    if(node == null){
+      return;
+    }else{
+      preOrderTraversal(node.left);
+      visit(node);
+      preOrderTraversal(node.right);
+    }
+
+  public void postOrderTraversal(Node node){
+    if(node == null){
+      return;
+    }else{
+      preOrderTraversal(node.left);
+      preOrderTraversal(node.right);
+      visit(node);
+    }
+  }
+
+
+  public void visit(Node node){
+    System.out.print(node.data + " ");
+  }
+
+
+
+  public static void main(String[] args) {
+    BinaryTree myTree = new BinaryTree();
+
+    myTree.add(10);
+    myTree.add(3);
+    myTree.add(20);
+    myTree.add(5);
+    myTree.add(30);
+    myTree.add(7);
+
+    myTree.preOrderTraversal(myTree.root);
+  }
 }
